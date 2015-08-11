23f7d0a380b629bb851443f03b8787708c93bce6
tomaszjanicki <tomaszjanicki@ciber.com>
2015-08-11 18:20:14 +0200

    changes

--- Architecture2.Common.Test/Helper.cs
+++ Architecture2.Common.Test/Helper.cs
@@ -1,4 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
 using FluentValidation.Results;
 
 namespace Architecture2.Common.Test
@@ -8,6 +14,57 @@ namespace Architecture2.Common.Test
         public static bool HasError(ValidationResult validationResult, string propertyName)
         {
             return validationResult.Errors.Count > 0 && validationResult.Errors.FirstOrDefault(failure => failure.PropertyName == propertyName) != null;
+        }
+
+        public static void IgnoreAwaitForNSubstituteAssertion(this Task task)
+        {
+
+        }
+
+        public static string GetResolvingErrors(IReadOnlyCollection<Type> types, Action<ContainerBuilder> registerDependenciesAction)
+        {
+            var errors = new List<string>();
+
+            var builder = new ContainerBuilder();
+
+            registerDependenciesAction(builder);
+
+            var container = builder.Build();
+
+            using (var lifetimeScope = container.BeginLifetimeScope())
+            {
+                foreach (var type in types)
+                {
+                    try
+                    {
+                        lifetimeScope.Resolve(type);
+                    }
+                    catch (DependencyResolutionException exception)
+                    {
+                        errors.Add(exception.Message);
+                    }
+                }
+            }
+            return string.Join(Environment.NewLine, errors);
+        }
+
+        public static IEnumerable<Type> GetTypes<T>(Assembly[] assemblies)
+        {
+            var types = new List<Type>();
+
+            foreach (var assembly in assemblies)
+                GetTypes<T>(assembly, types);
+            return types;
+        }
+
+        private static void GetTypes<T>(Assembly assembly, ICollection<Type> types)
+        {
+            // ReSharper disable once LoopCanBePartlyConvertedToQuery
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.Namespace != null && !type.IsAbstract && type.IsSubclassOf(typeof (T)))
+                    types.Add(type);
+            }
         }
 
     }
---
+++ Architecture2.Common.Web/AutofacExtension.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Owin;
+using Module = Autofac.Module;
+
+namespace Architecture2.Common.Web
+{
+    public static class AutofacExtension
+    {
+        public static IContainer ConfigureAutofacContainer(this IAppBuilder app, HttpConfiguration config, IReadOnlyCollection<Module> dependencyModules, WebOption webOption)
+        {
+            var assemblies = new[] { Assembly.GetCallingAssembly() };
+
+            return app.ConfigureAutofacContainer(config, assemblies, dependencyModules, webOption);
+        }
+
+        public static void ConfigureAutofacContainer(ContainerBuilder builder, WebOption webOption, params Assembly[] applicationAssemblies)
+        {
+            var assemblies = applicationAssemblies.ToArray();
+
+            if (webOption.UseMvc)
+            {
+                builder.RegisterControllers(assemblies);
+                builder.RegisterFilterProvider();
+                builder.RegisterSource(new ViewRegistrationSource());
+            }
+
+            if (webOption.UseApi)
+                builder.RegisterApiControllers(assemblies);
+        }
+
+        public static IContainer ConfigureAutofacContainer(this IAppBuilder app, HttpConfiguration config, IReadOnlyCollection<Assembly> applicationAssemblies, IEnumerable<Module> dependencyModules, WebOption webOption)
+        {
+            var builder = new ContainerBuilder();
+
+            foreach (var module in dependencyModules)
+            {
+                builder.RegisterModule(module);
+            }
+
+            var assemblies = applicationAssemblies.ToArray();
+
+            ConfigureAutofacContainer(builder, webOption, assemblies);
+
+            if (webOption.UseApi)
+                builder.RegisterWebApiFilterProvider(config);
+
+            var container = builder.Build();
+
+            if (webOption.UseApi)
+                config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            if (webOption.UseMvc)
+                DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            app.UseAutofacMiddleware(container);
+
+            if (webOption.UseApi)
+                app.UseAutofacWebApi(config);
+
+            return container;
+        }
+
+    }
+}
